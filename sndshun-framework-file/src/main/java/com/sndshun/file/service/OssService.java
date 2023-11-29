a4d16c614d9ad7191f63f9ea9895fb7ef84a0334
hlmy <yodc887@gmail>
2023-11-29 17:08:27 +0800

    继续完善文件上传功能模块

--- sndshun-framework-file/src/main/java/com/sndshun/file/service/OssService.java
+++ sndshun-framework-file/src/main/java/com/sndshun/file/service/OssService.java
@@ -3,6 +3,8 @@ package com.sndshun.file.service;
 
 import com.sndshun.commons.tools.Result;
 
+import java.io.InputStream;
+
 
 /**
  * 文件服务接口
@@ -33,4 +35,27 @@ public interface OssService {
      * @return 成功与否
      */
     Result<String> makeBucket(String bucketName);
+
+    /**
+     * 删除一个空桶
+     *
+     * @param bucketName 桶名称
+     * @return 成功与否
+     */
+    Result<String> removeBucket(String bucketName);
+
+    /**
+     * 上传文件
+     *
+     * @param inputStream      流
+     * @param originalFileName 原始文件名
+     * @param bucketName       桶名
+     * @return OssFile
+     */
+    Result<String> upload(InputStream inputStream, String bucketName, String originalFileName);
+
+    /**
+     * 初始化默认存储桶
+     */
+    void initDefaultBucket();
 }
--- sndshun-framework-file/src/main/java/com/sndshun/file/service/strategy/MinioOssServiceImpl.java
+++ sndshun-framework-file/src/main/java/com/sndshun/file/service/strategy/MinioOssServiceImpl.java
@@ -1,23 +1,28 @@
 package com.sndshun.file.service.strategy;
 
+import cn.hutool.core.date.DateUtil;
+import cn.hutool.core.util.StrUtil;
 import com.sndshun.commons.config.ResultCode;
 import com.sndshun.commons.tools.Result;
 import com.sndshun.commons.tools.StringUtils;
 import com.sndshun.file.config.OssProperties;
+import com.sndshun.file.entity.OssFile;
 import com.sndshun.file.service.OssService;
-import io.minio.BucketExistsArgs;
-import io.minio.MakeBucketArgs;
-import io.minio.MinioClient;
+import io.minio.*;
 import io.minio.errors.*;
 import io.minio.messages.Bucket;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
 import java.io.IOException;
+import java.io.InputStream;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
+import java.util.Date;
 import java.util.List;
+import java.util.UUID;
 
 /**
  * Minio 对象存储服务
@@ -62,7 +67,7 @@ public class MinioOssServiceImpl implements OssService {
     public Result<String> bucketExists(String bucketName) {
         try {
             log.info("MinioOssServiceImpl bucketExists start");
-            boolean result = minioClient.bucketExists(BucketExistsArgs.builder().bucket(bucketName).build());
+            boolean result = bucketExist(bucketName);
             log.info("MinioOssServiceImpl bucketExists end");
             return result ? Result.ok(ResultCode.OSS_BUCKET_EXIST) : Result.error(ResultCode.OSS_BUCKET_IS_EXIST);
         } catch (Exception e) {
@@ -75,7 +80,7 @@ public class MinioOssServiceImpl implements OssService {
     public Result<String> makeBucket(String bucketName) {
         try {
             log.info("MinioOssServiceImpl makeBucket start");
-            boolean result = minioClient.bucketExists(BucketExistsArgs.builder().bucket(bucketName).build());
+            boolean result = bucketExist(bucketName);
             StringUtils.isBooleanTrue(result);
             minioClient.makeBucket(MakeBucketArgs.builder().bucket(bucketName).build());
             log.info("MinioOssServiceImpl makeBucket end");
@@ -84,5 +89,82 @@ public class MinioOssServiceImpl implements OssService {
             log.error(e.getLocalizedMessage());
             return Result.error(ResultCode.ERROR);
         }
+    }
+
+    @Override
+    public Result<String> removeBucket(String bucketName) {
+        try {
+            log.info("MinioOssServiceImpl makeBucket start");
+            RemoveBucketArgs removeBucketArgs = RemoveBucketArgs.builder().bucket(bucketName).build();
+            minioClient.removeBucket(removeBucketArgs);
+            log.info("MinioOssServiceImpl makeBucket end");
+            return Result.ok(ResultCode.OSS_DEL_OK);
+        } catch (Exception e) {
+            log.error(e.getLocalizedMessage());
+            log.info("MinioOssServiceImpl makeBucket end");
+            return Result.error(ResultCode.OSS_DEL_ERROR);
+        }
+    }
+
+    @Override
+    public Result<String> upload(InputStream inputStream, String bucketName, String originalFileName) {
+        String uuidFileName = generateOssUuidFileName(originalFileName);
+        try {
+            if (StrUtil.isEmpty(bucketName)) {
+                bucketName = ossProperties.getDefaultBucketName();
+            }
+            minioClient.putObject(PutObjectArgs.builder().bucket(bucketName).object(uuidFileName).stream(inputStream, inputStream.available(), -1).build());
+            OssFile ossFile = new OssFile(uuidFileName, originalFileName);
+            return Result.ok(ResultCode.OSS_UPLOAD_OK, ossFile.toString());
+        } catch (Exception e) {
+            log.error(e.getLocalizedMessage());
+            return Result.error(ResultCode.OSS_UPLOAD_ERROR);
+        } finally {
+            if (inputStream != null) {
+                try {
+                    inputStream.close();
+                } catch (Exception e) {
+                    log.error(e.getLocalizedMessage());
+                }
+            }
+        }
+    }
+
+    @Override
+    @PostConstruct
+    public void initDefaultBucket() {
+        try {
+            log.info("MinioOssServiceImpl initDefaultBucket start");
+            String defaultBucketName = ossProperties.getDefaultBucketName();
+            boolean exist = bucketExist(defaultBucketName);
+            StringUtils.isBooleanTrue(exist);
+            /*不存在手动创建*/
+            makeBucket(ossProperties.getDefaultBucketName());
+            log.info("MinioOssServiceImpl initDefaultBucket end");
+        } catch (Exception e) {
+            log.error(e.getLocalizedMessage());
+        }
+    }
+
+
+    /**
+     * 验证桶名称是否存在
+     *
+     * @param bucketName 桶名称
+     * @return 真假
+     * @throws Exception 异常
+     */
+    private boolean bucketExist(String bucketName) throws Exception {
+        return minioClient.bucketExists(BucketExistsArgs.builder().bucket(bucketName).build());
+    }
+
+    /**
+     * 生成随机文件名，防止重复
+     *
+     * @param originalFilename 原始文件名
+     * @return
+     */
+    public String generateOssUuidFileName(String originalFilename) {
+        return "files" + StrUtil.SLASH + DateUtil.format(new Date(), "yyyy-MM-dd") + StrUtil.SLASH + UUID.randomUUID() + StrUtil.SLASH + originalFilename;
     }
 }
