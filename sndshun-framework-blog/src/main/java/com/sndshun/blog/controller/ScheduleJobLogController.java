8ffc769dedcc92f37fa70f1dee3318c6462b926b
sndshun <sndshun@foxmail>
2023-12-13 14:23:27 +0800

    定时任务日志

---
+++ sndshun-framework-blog/src/main/java/com/sndshun/blog/controller/ScheduleJobLogController.java
@@ -0,0 +1,96 @@
+package com.sndshun.blog.controller;
+
+
+import com.sndshun.commons.tools.Result;
+import com.sndshun.blog.entity.ScheduleJobLogEntity;
+import com.sndshun.blog.service.ScheduleJobLogService;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * 日志(ScheduleJobLog)表控制层
+ *
+ * @author sndshun
+ * @since 2023-12-13 14:22:47
+ */
+@RestController
+@RequestMapping("scheduleJobLog")
+public class ScheduleJobLogController {
+    /**
+     * 服务对象
+     */
+    @Resource
+    private ScheduleJobLogService scheduleJobLogService;
+
+    /**
+     * 分页查询所有数据
+     *
+     * @param page           分页对象
+     * @param scheduleJobLog 查询实体
+     * @return 所有数据
+     */
+    @GetMapping
+    public Result<Page<ScheduleJobLogEntity>> selectPage(Page<ScheduleJobLogEntity> page, ScheduleJobLogEntity scheduleJobLog) {
+        return Result.ok(this.scheduleJobLogService.page(page, new QueryWrapper<>(scheduleJobLog)));
+    }
+
+    /**
+     * 通过主键查询单条数据
+     *
+     * @param id 主键
+     * @return 单条数据
+     */
+    @GetMapping("{id}")
+    public Result<ScheduleJobLogEntity> selectOne(@PathVariable Serializable id) {
+        return Result.ok(this.scheduleJobLogService.getById(id));
+    }
+
+    /**
+     * 新增数据
+     *
+     * @param scheduleJobLog 实体对象
+     * @return 新增结果
+     */
+    @PostMapping
+    public Result<Boolean> insert(@RequestBody ScheduleJobLogEntity scheduleJobLog) {
+        return Result.ok(this.scheduleJobLogService.save(scheduleJobLog));
+    }
+
+    /**
+     * 修改数据
+     *
+     * @param scheduleJobLog 实体对象
+     * @return 修改结果
+     */
+    @PutMapping
+    public Result<Boolean> update(@RequestBody ScheduleJobLogEntity scheduleJobLog) {
+        return Result.ok(this.scheduleJobLogService.updateById(scheduleJobLog));
+    }
+
+    /**
+     * 删除数据
+     *
+     * @param id 主键
+     * @return 删除结果
+     */
+    @DeleteMapping("/{id}")
+    public Result<Boolean> delete(@PathVariable Long id) {
+        return Result.ok(this.scheduleJobLogService.removeById(id));
+    }
+
+    /**
+     * 删除数据
+     *
+     * @param idList 主键结合
+     * @return 删除结果
+     */
+    @DeleteMapping("batch")
+    public Result<Boolean> deleteBatch(@RequestBody List<Long> idList) {
+        return Result.ok(this.scheduleJobLogService.removeByIds(idList));
+    }
+}
