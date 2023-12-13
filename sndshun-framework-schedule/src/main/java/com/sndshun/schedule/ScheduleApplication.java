b16e10a6da14c56085f789c279b8a4028203c2cd
hlmy <yodc887@gmail>
2023-12-13 10:44:05 +0800

    添加定时任务模块

---
+++ sndshun-framework-schedule/src/main/java/com/sndshun/schedule/ScheduleApplication.java
@@ -0,0 +1,17 @@
+package com.sndshun.schedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 定时任务
+ *
+ * @author maple
+ */
+@SpringBootApplication
+public class ScheduleApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ScheduleApplication.class, args);
+    }
+
+}
