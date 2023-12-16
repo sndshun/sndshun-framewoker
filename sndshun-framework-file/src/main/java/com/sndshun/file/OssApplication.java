91d2ef3984b96da35ac3ec6ec92ba8675542db5b
sndshun <sndshun@foxmail>
2023-12-16 17:22:20 +0800

    blog集成文件上传

--- sndshun-framework-file/src/main/java/com/sndshun/file/OssApplication.java
+++ sndshun-framework-file/src/main/java/com/sndshun/file/OssApplication.java
@@ -1,5 +1,6 @@
 package com.sndshun.file;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author mapleie
  */
 @SpringBootApplication(scanBasePackages = "com.sndshun")
+@MapperScan("com.sndshun.file.mapper")
 public class OssApplication {
     public static void main(String[] args) {
         SpringApplication.run(OssApplication.class, args);
